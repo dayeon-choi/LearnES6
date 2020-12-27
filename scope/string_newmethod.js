73a04d391aae3c7ef2321f9de7558b5e21897803
dayeon-choi <dayeon2399@naver.com>
2020-12-27 13:56:10 +0900

    dayeon's commit - scope done :)

--- scope/string_newmethod.js
+++
@@ -1,7 +0,0 @@
-//ES2015 String에 새로운 메서드들
-
-let str = "hello world ! ^^ ~~";
-let matchstr = "hello";
-console.log(str.startsWith(matchstr));  //true
-console.log(str.endsWith(matchstr));    //false
-console.log("include test ", str.includes("^"));    //include test true
