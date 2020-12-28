87e6c266046a95669bf3a3e008fd62299654ca86
dayeon-choi <dayeon2399@naver.com>
2020-12-28 23:52:30 +0900

    dayeon's commit - ex01 done ;)

--- ex01/ex01.js
+++ ex01/ex01.js
@@ -3,7 +3,12 @@ filter, includes, from을 사용해서
 문자열 'e'가 포함된 노드로 구성된 배열을 만들어서 반환하기
 */
 function print(){
-    
+    let list = document.querySelectorAll("li");
+    let listArray = Array.from(list);   //li노드로 구성된 배열
+    let eArray = listArray.filter(function(v){
+        return v.innerText.includes("e");
+    });
+    return eArray;
 }
 
-print();
+console.log(print());
