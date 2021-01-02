f49ee304bbb6fbf99375e472da9be4345791dc50
dayeon-choi <dayeon2399@naver.com>
2021-01-02 13:39:23 +0900

    dayeon's commit - Tagged Template literals done :)

--- template/tagged_template_literals.js
+++ template/tagged_template_literals.js
@@ -14,16 +14,23 @@ const data = [
     {
         name : 'starbucks',
         order : false,
+    },
+    {
+        name : 'coffee-king',
+        order : true,
+        items : ['americano','latte']
     }
 ]
 
 function fn(tags,name,items){
-    console.log(tags);
-
+    if(typeof items === "undefined"){
+        items = "주문가능한 상품이 없습니다.";
+    }
+    return (tags[0] + name + tags[1] + items + tags[2]);
 }
 
-const template = `<div>welcome ${data[1].name}!!</div>`+`\n`+
-                `<h2>주문가능항목</h2>`+`\n`+
-                `<div>${data[1].items}</div>`;
-
-console.log(template);
+data.forEach((v) => {
+    //template literals ``
+    let template = fn`<div>welcome ${v.name}!!</div><h2>주문가능항목</h2><div>${v.items}</div>`;
+    document.querySelector("#message").innerHTML += template;
+});
